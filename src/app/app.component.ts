e036b998428c58c0dd98e4976bba89821a859ff0
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-05-22 23:02:14 +0530

    Learn reactive form in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'yuotube';
+export class AppComponent implements OnInit {
+  userform!:FormGroup
+  email = new FormControl('')
+
+  constructor(private readonly fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.initiateForm()
+  }
+
+
+
+  initiateForm(){
+    this.userform = this.fb.group({
+      email : ['',Validators.required,Validators.maxLength(10)],
+      password : ['',Validators.required],
+      address : ['',Validators.required],
+      city : ['',Validators.required],
+      district : ['',Validators.required],
+    })
+  }
+
+  submit(){
+    // if (this.userform.invalid) {
+    //   alert('Invalid from')
+    //   return
+    // }
+
+    console.log(this.userform.value);
+    
+    
+  }
+  
 }
